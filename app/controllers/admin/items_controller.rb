670fb411ccb06a231844b66aa1ec04dd6085d407
Magomed Saipudinov <saipudinov.magomed@gmail.com>
2019-01-30 18:13:57 +0300

    Fixed bug when unsuccessful created Item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,25 @@
 module Admin
   class ItemsController < AdminController
+    before_action :set_new_item
+
     def index
       @items = Item.all
     end
 
     def new
-      @item = Item.new
       @item.item_photos.build
     end
 
     def create
-      @item = Item.create(permit_params)
-      redirect_to(admin_items_path)
+      @item.assign_attributes(permit_params)
+      @item.save ? redirect_to(admin_items_path) : render(:new)
     end
 
     private
+
+    def set_new_item
+      @item = Item.new
+    end
 
     def permit_params
       params.require(:item).permit(:name, :description, :price, :category_id, item_photos_attributes: [:photo])
