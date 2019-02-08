406f38121c8793c116cc0a2a48fcf0d6fc7b45f8
Magomed Saipudinov <saipudinov.magomed@gmail.com>
2019-02-08 06:54:40 +0300

    Changed seeds file for create Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 # Users created
-User.create(first_name: 'Test', last_name: 'Admin', email: 'admin@example.com', password: '123456', role: 0)
-User.create(first_name: 'Test', last_name: 'User', email: 'user@example.com', password: '123456')
+User.create(first_name: 'Test', last_name: 'Super', email: 'super@example.com', password: '123456', role: 0)
+User.create(first_name: 'Test', last_name: 'Admin', email: 'admin@example.com', password: '123456', role: 1)
+1.upto(20) do |i|
+  User.create(first_name: "Test-#{i}", last_name: "User-#{i}", email: "user_#{i}@example.com", password: '123456')
+end
 puts '-- Users created'
 
 # Categories created
@@ -13,6 +16,7 @@ puts '-- Categories created'
 Category.all.each_with_index do |category, index|
   1.upto(20) do |i|
     category.items.create(name: "Item-#{index + 1}-#{i}", description: 'This item from db/seeds', price: i * 10)
+    puts "-- Item-#{index + 1}-#{i} created"
   end
 end
 puts '-- Items created'
