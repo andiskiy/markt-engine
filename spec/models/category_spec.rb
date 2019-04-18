705e8eaf5e3346eb2d7e5211b3adf1bf3917c7d2
Magomed Saipudinov <saipudinov.magomed@gmail.com>
2019-04-18 04:23:20 +0300

    Added tests for Category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    let(:first_items)     { create_list(:item, 100, category: first_category).sort_by(&:name) }
+    let(:second_items)    { create_list(:item, 100, category: second_category).sort_by(&:name) }
+    let(:first_category)  { create :category }
+    let(:second_category) { create :category }
+
+    it 'items for first_category' do
+      expect(first_category.items).to eq(first_items)
+    end
+
+    it 'items for second_category' do
+      expect(second_category.items).to eq(second_items)
+    end
+  end
+
+  describe 'validations' do
+    it 'name presence' do
+      expect(build(:category, name: nil)).not_to be_valid
+    end
+  end
+
+  describe 'scopes' do
+    let(:name)       { 'AAAAAAAA' }
+    let(:category)   { create :category, name: name }
+    let(:categories) { create_list(:category, 100).sort_by(&:name) }
+
+    it 'search' do
+      categories
+      category
+      expect(described_class.search(name)).to eq([category])
+    end
+
+    it 'order by' do
+      categories
+      category
+      expect(described_class.all.order_by_name.first).to eq(category)
+    end
+  end
 end
